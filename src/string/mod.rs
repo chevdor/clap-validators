add2e4071be4e765b75ac90ffade4746f8c9c2db
Will <w.kopp@kigroup.de>
2018-07-19 13:37:35 +0200

    Add string validator is_shorter_than

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -7,6 +7,14 @@ pub fn is_longer_than(s: String, l: usize) -> Result<(), String> {
     Err(format!("{} has {} chars so it is less than {} chars", s, len, l))
 }
 
+/// Ensure a string is short enough
+pub fn is_shorter_than(s: String, l: usize) -> Result<(), String> {
+    let len = s.chars().count();
+    if len < l {
+    	return Ok(())
+    }
+    Err(format!("{} has {} chars so it is more than {} chars", s, len, l))
+}
 
 #[cfg(test)]
 mod tests {
@@ -20,5 +28,15 @@ mod tests {
     #[test]
     fn test_is_longer_than_false() {
         assert!(is_longer_than("short".to_string(), 10).is_err());
+    }
+
+    #[test]
+    fn test_is_shorter_than_true() {
+        assert!(is_shorter_than("ラウトは難しいです！".to_string(), 30).is_ok());
+    }
+
+    #[test]
+    fn test_is_shorter_than_false() {
+        assert!(is_shorter_than("short".to_string(), 3).is_err());
     }
  }
